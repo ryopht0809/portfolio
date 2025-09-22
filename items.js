23832d23797ec2fda364255b7884c6092149bb5b
ryopht0809 <sonokka14@gmail.com>
2025-09-23 00:54:12 +0900

    Create items.js

---
+++ items.js
@@ -0,0 +1,15 @@
+// items.js
+window.ITEMS = [
+  {
+    id: "1",
+    src: "./img/ryo-001.jpg",
+    title: "Hongdae Sunset",
+    // 선택(안 쓰면 기본값): 날짜/계정
+    date: "2025-10-12",
+    x: { handle: "@ryo_pht", url: "https://x.com/ryo_pht/status/123" },
+    // 선택: 태그/EXIF
+    tags: ["street","portrait","Seoul"],
+    exif: { camera:"X-T30 II", lens:"35/1.7", focal:"35mm", shutter:"1/250", aperture:"f/2.0", iso:"400", date:"2025-10-12" }
+  },
+  // 계속 추가...
+];
