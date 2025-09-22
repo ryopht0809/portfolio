f7606698670b98b8ea2eeb22c5a2292903d85c3f
ryopht0809 <sonokka14@gmail.com>
2025-09-23 01:22:03 +0900

    Update items.js

--- items.js
+++ items.js
@@ -1,13 +1,13 @@
 // items.js
 window.ITEMS = [
-  {
-    id: "1",
+      id: "1",
     src: "./portfolio/img/그랜라간 리턴.jpeg",
     title: "그랜라간 시몬과 카미나",
     // 선택(안 쓰면 기본값): 날짜/계정
     date: "2025-10-12",
     x: { handle: "@SteinsAnime", url: "https://x.com/SteinsAnime" },
     x: { handle: "@malgom_cos", url: "https://x.com/malgom_cos" },
+  {
     // 선택: 태그/EXIF
   },
   // 계속 추가...
