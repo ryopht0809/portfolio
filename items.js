0edeb78862dc3d1f338fa628566dc4c0fefb7c15
ryopht0809 <sonokka14@gmail.com>
2025-09-23 01:24:37 +0900

    Update items.js

--- items.js
+++ items.js
@@ -1,14 +1,12 @@
-// items.js
 window.ITEMS = [
-      id: "1",
-    src: "./portfolio/img/granlagan-return.jpg",
-    title: "그랜라간 시몬과 카미나",
-    // 선택(안 쓰면 기본값): 날짜/계정
-    date: "2025-10-12",
-    x: { handle: "@SteinsAnime", url: "https://x.com/SteinsAnime" },
-    x: { handle: "@malgom_cos", url: "https://x.com/malgom_cos" },
   {
-    // 선택: 태그/EXIF
+    id: "1",
+    src: "./img/granlagan-return.jpg",
+    title: "그랜라간 시몬과 카미나",
+    date: "2025-10-12",
+    x: [
+      { handle: "@SteinsAnime",  url: "https://x.com/SteinsAnime" },
+      { handle: "@malgom_cos",   url: "https://x.com/malgom_cos" }
+    ],
   },
-  // 계속 추가...
 ];
