c8aa15cd7c4b8f11a808d1b7651a9026594db196
ryopht0809 <sonokka14@gmail.com>
2025-09-23 01:33:41 +0900

    Update items.js

--- items.js
+++ items.js
@@ -2,10 +2,9 @@
 window.ITEMS = [
   {
     id: "1",
-    src: "./portfolio/img/granlagan-return.jpg",   // 실제로 존재하는 파일명/경로로!
+    src: "./img/granlagan-return.jpg",   // ← 경로 수정 (확장자 정확히!)
     title: "그랜라간 시몬과 카미나",
     date: "2025-10-12",
     x: { handle: "@SteinsAnime", url: "https://x.com/SteinsAnime" },
-    // tags/exif는 없어도 됨
-  }
+  },
 ];
