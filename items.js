6922fa444f115f6a1458dbaf96f15d939539ae69
ryopht0809 <sonokka14@gmail.com>
2025-09-23 01:18:25 +0900

    Update items.js

--- items.js
+++ items.js
@@ -2,14 +2,13 @@
 window.ITEMS = [
   {
     id: "1",
-    src: "./img/ryo-001.jpg",
-    title: "Hongdae Sunset",
+    src: "./portfolio/img/그랜라간 리턴.jpeg",
+    title: "그랜라간 시몬과 카미나",
     // 선택(안 쓰면 기본값): 날짜/계정
     date: "2025-10-12",
-    x: { handle: "@ryo_pht", url: "https://x.com/ryo_pht/status/123" },
+    x: { handle: "@SteinsAnime", url: "https://x.com/SteinsAnime" },
+    x: { handle: "@malgom_cos", url: "https://x.com/malgom_cos" },
     // 선택: 태그/EXIF
-    tags: ["street","portrait","Seoul"],
-    exif: { camera:"X-T30 II", lens:"35/1.7", focal:"35mm", shutter:"1/250", aperture:"f/2.0", iso:"400", date:"2025-10-12" }
   },
   // 계속 추가...
 ];
