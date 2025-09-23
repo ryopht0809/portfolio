dabcbd7e9a16398a0c44fb677e6a313f592d4465
ryopht0809 <sonokka14@gmail.com>
2025-09-24 00:36:38 +0900

    Create items.js

---
+++ items.js
@@ -0,0 +1,11 @@
+// items.js
+window.ITEMS = [
+  {
+    id: "1",
+    src: "./img/granlagan-return.jpg",
+    title: "그랜라간 시몬과 카미나",
+    date: "2025-10-12",
+    // tags: ["cosplay", "anime"],
+    // exif: { camera:"", lens:"", focal:"", shutter:"", aperture:"", iso:"", date:"2025-10-12" }
+  }
+];
