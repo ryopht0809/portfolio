f01cd1fe53cbce845f7b4a997693837997437c37
ryopht0809 <sonokka14@gmail.com>
2025-09-24 01:26:54 +0900

    Update items.js

--- items.js
+++ items.js
@@ -4,9 +4,8 @@ window.ITEMS = [
     id: "1",
     src: "./img/granlagan-return.jpeg.jpeg",
     title: "그랜라간 시몬과 카미나",
-    date: "2025-10-12",
+    date: "2025-09-13",
     ratio: "2/3",        // 세로 사진
-    hero: true,          // ← 이 아이템이 '대표사진'
     models: [
       { name: "말곰님", handle: "@malgom_cos", url: "https://x.com/malgom_cos" },
       { name: "슈타님", handle: "@SteinsAnime", url: "https://x.com/SteinsAnime" }
@@ -16,10 +15,10 @@ window.ITEMS = [
   },
   {
     id: "2",
-    src: "./img/sample-horizontal.jpg",
-    title: "홍대 스트릿",
-    date: "2025-10-10",
+    src: "./img/250921 1.jpg",
+    title: "강철의 연금술사",
+    date: "2025-09-21",
     ratio: "3/2",        // 가로 사진
-    models: [{ handle: "@ryo_pht", url: "https://x.com/ryo_pht" }]
+    hero: true,          // ← 이 아이템이 '대표사진'
   }
 ];
